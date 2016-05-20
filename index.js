9df3cb62261515360df3895e7360a2c01b4e5d01
Joe <josepha48@gmail.com>
2016-05-20 11:22:03 -0700

    initial index.js file for node server

---
+++ index.js
@@ -0,0 +1,84 @@
+var express = require( 'express' ),
+    server,
+    secureServer,
+    args = process.argv,
+    bodyParser = require( 'body-parser' ),
+    winston = require( 'winston' ),
+    exec = require( "child_process" ).exec,
+    exphbs = require( 'express-handlebars' ),
+    app = express(),
+    https = require( 'https' ),
+    http = require( 'http' ),
+    fs = require( "fs" ),
+    clientCode,
+    clientConfig,
+    config;
+
+app.engine( '.hbs', exphbs( {
+    defaultLayout: 'baseLayout',
+    extname: '.hbs'
+} ) );
+
+app.set( 'view engine', '.hbs' );
+
+// for parsing application/json
+app.use( bodyParser.json() );
+// for parsing application/x-www-form-urlencoded
+app.use( bodyParser.urlencoded( {
+    extended: true
+} ) );
+
+// read config
+clientCode = args[ 2 ];
+clientConfig = args[ 3 ];
+config = JSON.parse( fs.readFileSync( clientConfig ) );
+
+function sendResponse( res, toggle ) {
+    var result, cmd;
+    cmd = "node " + clientCode + " " + clientConfig;
+    if ( typeof toggle !== 'undefined' ) {
+        cmd = cmd + " " + toggle;
+    }
+    result = exec( cmd, function ( err, stdout, stderr ) {
+        var result = '';
+        if ( stdout ) {
+            result += stdout;
+        }
+        if ( stderr ) {
+            result += stderr;
+        }
+        if ( err ) {
+            result += err;
+        }
+        if ( result ) {
+            result = result.replace( "Done!", "" );
+            result = JSON.parse( result );
+            res.render( 'index', {
+                "currentState": result[ config.replyMessage ]
+            } );
+        } else {
+            res.render( 'index', {
+                "currentState": "error"
+            } );
+        }
+    } );
+}
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get( '/', function ( req, res ) {
+    sendResponse( res );
+} );
+
+// deal wtih a post
+app.post( '/update', function ( req, res ) {
+    var updateKey;
+    winston.log( 'info', "Key = " + req.body.changeModeKey );
+    if ( req.body.changeModeKey && req.body.changeModeKey == config.changeModeKey ) {
+        updateKey = req.body.changeModeKey;
+    }
+    sendResponse( res, updateKey );
+} );
+
+secureServer = https.createServer(options, app);
+secureServer.listen(config.port);
+winston.log( 'info', "Listening on port ${config.port}, not secure!" );
