0edcdc576a3a0c87dd9d9fc3920b3e43ead4fecb
Joe <josepha48@yahoo.com>
2018-04-10 19:10:02 -0700

    update iptables

--- iptables.sh
+++ iptables.sh
@@ -2,12 +2,14 @@
 
 # WIP - goal is to make a script that can be used for the firewall on this access point 
 
+#set -x 
+
 ACCESS_POINT_IFACE=wlan0
 PUBLIC_IFACE=wlan1
 
 # short hand 
-LAN_IFACE=$ACCESS_POINT_INTERFACE
-WAN_IFACE=$PUBLIC_INTERFACE
+LAN_IFACE=$ACCESS_POINT_IFACE
+WAN_IFACE=$PUBLIC_IFACE
 # flush all rules
 iptables -F 
 ip6tables -F
@@ -15,10 +17,13 @@ ip6tables -F
 # enable ssh so you can talk to the pi :) 
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 
+# enable anything on the internal interface, because it should be secured by wpa key
+iptables -A INPUT -i $LAN_IFACE -j ACCEPT
+
 # default polices
 # enable these when you have everything else working 
-# iptables -P INPUT DROP
-# iptables -P FORWARD DROP
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
 # iptables -P OUTPUT ACCEPT
 
 # loopback 
@@ -37,8 +42,8 @@ iptables -A FORWARD -o $LAN_IFACE -j ACCEPT
 iptables -t nat -A POSTROUTING -o $WAN_IFACE -j MASQUERADE
 
 # so for an access point we need to enable dhcpd ports 
-#iptables -A INPUT -i $API -p udp --dport 67 -j ACCEPT
-#iptables -A INPUT -i $API -p udp --dport 68 -j ACCEPT
+iptables -A INPUT -i $LAN_IFACE -p udp --dport 67 -j ACCEPT
+iptables -A INPUT -i $LAN_IFACE -p udp --dport 68 -j ACCEPT
 
 # save rules 
 # /sbin/service iptables save
