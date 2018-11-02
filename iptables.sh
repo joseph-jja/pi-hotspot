e2169f6c756fd87df336a91bf497432111a30f22
Joe <josepha48@gmail.com>
2018-11-01 22:15:05 -0700

    update rules
    simplify, log and restrict

--- iptables.sh
+++ iptables.sh
@@ -26,38 +26,38 @@ iptables -P INPUT DROP
 iptables -P FORWARD DROP
 # iptables -P OUTPUT ACCEPT
 
-# enable ssh so you can talk to the pi :) 
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-
-# enable anything on the internal interface, because it should be secured by wpa key
-iptables -A INPUT -i $LAN_IFACE -j ACCEPT
-
 # loopback 
 iptables -A INPUT -i lo -j ACCEPT
+
+# enable ssh so you can talk to the pi :) 
+iptables -A INPUT -p tcp --dport 22 -m state --state NEW -j LOG
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+
+# dhcpd ports 
+iptables -A INPUT ! -i lo -p udp -m multiport --dport 67,68 -j ACCEPT
+
+# dns requests 
+iptables -A INPUT ! -i lo -p tcp --dport 53 -j ACCEPT
+iptables -A INPUT ! -i lo -p udp --dport 53 -j ACCEPT
+
+# accept new connections on these interfaces
+iptables -A INPUT -i $LAN_IFACE -j ACCEPT -m state --state NEW
 
 # accept established or 'related' connections 
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
+# garbage on the network 
+iptables -A INPUT -j DROP -p udp -m multiport --dport 2190,7788
+
+# enable anything on the internal interface
+# might not need this?
+iptables -A INPUT -i $LAN_IFACE -j ACCEPT
+
 # forward packets between interfaces
-iptables -A FORWARD -i $LAN_IFACE -j ACCEPT
-iptables -A FORWARD -o $LAN_IFACE -j ACCEPT
+iptables -A FORWARD ! -i lo -j ACCEPT
 
 # need nat too
 iptables -t nat -A POSTROUTING -o $WAN_IFACE -j MASQUERADE
-
-# so for an access point we need to enable dhcpd ports 
-iptables -A INPUT -i $LAN_IFACE -p udp --dport 67 -j ACCEPT
-iptables -A INPUT -i $LAN_IFACE -p udp --dport 68 -j ACCEPT
-iptables -A INPUT -i $WAN_IFACE -p udp --dport 67 -j ACCEPT
-iptables -A INPUT -i $WAN_IFACE -p udp --dport 68 -j ACCEPT
-
-# dns requests on lan interface
-iptables -A INPUT -i $LAN_IFACE -p tcp --dport 53 -j ACCEPT
-iptables -A INPUT -i $LAN_IFACE -p udp --dport 53 -j ACCEPT
-
-# garbage on my network 
-iptables -A INPUT -j DROP -p udp --dport 2190
-iptables -A INPUT -j DROP -p udp --dport 7788
 
 # at last logging
 iptables -A INPUT -j LOG -m limit --limit 1/min 
@@ -67,3 +67,4 @@ iptables -A FORWARD -j DROP
 
 # save rules 
 # /sbin/service iptables save
+
